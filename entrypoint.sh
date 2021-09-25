811f001e2c045ebe7c4db97aa333969075d043b6
Amar Sharma <amar-sharma@users.noreply.github.com>
2021-09-25 22:07:38 +0530

    Use Firebase project

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ if [ -z "${FIREBASE_TOKEN}" ]; then
     echo "FIREBASE_TOKEN is missing"
     exit 1
 fi
-
+cd functions && yarn
+firebase use ${FIREBASE_PROJECT}
 firebase deploy \
     --token ${FIREBASE_TOKEN} \
     --only functions,firestore,storage
